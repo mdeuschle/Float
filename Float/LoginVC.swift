31d59f1eb9e9d8ad463c46ba67e2499295cf6567
Matt Deuschle <mdeuschle@gmail.com>
2017-01-29 14:43:52 -0800

    completed UI for login

--- Float/LoginVC.swift
+++ Float/LoginVC.swift
@@ -8,19 +8,21 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var errorLabel: UILabel!
     @IBOutlet var skipButton: UIButton!
     @IBOutlet var facebookButton: UIButton!
-    
+    @IBOutlet var appLogoImage: UIImageView!
+    @IBOutlet var appTaglineLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         stylizeViews()
+        notifications()
     }
 
     func stylizeViews() {
@@ -28,8 +30,48 @@ class LoginVC: UIViewController {
         facebookButton.layer.borderColor = UIColor.accentColor().cgColor
     }
 
+    func notifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginVC.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginVC.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("*Textfield Return")
+        return true
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        appLogoImage.isHidden = false
+        appTaglineLabel.isHidden = false
+        skipButton.isHidden = false
+        view.endEditing(true)
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        appLogoImage.isHidden = true
+        appTaglineLabel.isHidden = true
+        skipButton.isHidden = true
+    }
+
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if view.frame.origin.y == 0{
+                view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if view.frame.origin.y != 0{
+                view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
+
     @IBAction func skipButtonTapped(_ sender: Any) {
     }
+    
     @IBAction func facebookButtonTapped(_ sender: Any) {
     }
 
