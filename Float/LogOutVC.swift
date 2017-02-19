261ef41e7fc524432a7c6897d44ea5d570fed7d4
Matt Deuschle <mdeuschle@gmail.com>
2017-02-18 17:58:51 -0800

    remove from keychain after logout

--- Float/LogOutVC.swift
+++ Float/LogOutVC.swift
@@ -7,22 +7,28 @@
 //
 
 import UIKit
+import Firebase
+import SwiftKeychainWrapper
 
 class LogOutVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
-
-
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        dismiss(animated: true, completion: nil)
+        logout()
 
     }
 
-
- 
+    func logout() {
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey: Constant.KeyType.keyUID)
+        print("*Removed keychain: \(keychainResult)")
+        do {
+            try FIRAuth.auth()?.signOut()
+            self.dismiss(animated: true, completion: nil)
+        } catch {
+            print("Unable to sign out \(error)")
+        }
+    }
 }
